e1456fc4cd86846346e2d961edacc64a1065a1e5
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-03-21 10:21:53 +0530

    implementation of filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState();
+  const [restaurants, setRestaurants] = useState(null);
+  const [filteredRestaurants, setFilterRestaurants] = useState(null);
+  const [searchText, setSerachText] = useState();
 
   useEffect(() => {
     fetchData();
@@ -13,26 +15,64 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
+
     setRestaurants(
       jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+    setFilterRestaurants(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
-  return (
+
+  return restaurants === null ? (
+    <h1>Restaurant are not listed</h1>
+  ) : (
     <>
       <div className="flex">
         <div className="my-4">
-          <input type="text" name="" id="" className="border" />
+          <input
+            type="text"
+            name=""
+            id=""
+            className="border"
+            value={searchText}
+            onChange={(e) => setSerachText(e.target.value)}
+          />
+          <button
+            className=" rounded bg-cyan-200 px-4 w-20 m-2 h-6 "
+            onClick={() => {
+              const filteredList = restaurants.filter((res) =>
+                res?.info?.name
+                  ?.toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilterRestaurants(filteredList);
+            }}
+          >
+            {" "}
+            Search
+          </button>
+          <button
+            className="bg-cyan-100 rounded  px-2 "
+            onClick={() => {
+              const filterByStar = restaurants.filter(
+                (restaurant) => restaurant?.info?.avgRating >= 4.5
+              );
+              setFilterRestaurants(filterByStar);
+            }}
+          >
+            Above 4.5 🌠
+          </button>
         </div>
-        <button className=" rounded bg-cyan-200 px-4 w-20 m-2 h-6  ">
-          {" "}
-          Search
-        </button>
       </div>
-      <div>
-        {/* {restaurants.map((restaurant) => {
-          <RestaurantList data={restaurant} />;
-        })} */}
+      <div className="flex flex-wrap ml-10 ">
+        {filteredRestaurants.map((restaurant) => {
+          return (
+            <RestaurantList key={restaurant?.info?.id} data={restaurant} />
+          );
+        })}
       </div>
     </>
   );
--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable react/prop-types */
 const RestaurantList = ({ data }) => {
-  console.log(data);
-
+  // console.log(data);
+  // console.log(data?.info?.name);
+  const { name, avgRating, cuisines, cloudinaryImageId } = data?.info;
   return (
     <>
-      <div>restaurant list</div>
+      <div className="w-80 bg-cyan-200 m-2 rounded">
+        <div className="flex justify-center align-middle rounded">
+          <img
+            src={
+              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+              cloudinaryImageId
+            }
+            alt=""
+            className="h-40"
+          />
+        </div>
+        <div className="text-lg font-bold">{name}</div>
+        <div className="truncate ">{cuisines.join(" ,")}</div>
+        <div>{avgRating}</div>
+      </div>
     </>
   );
 };
